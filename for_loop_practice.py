2d3422318cd0fcf4efbbf023f2d732c6a6ee190c
aliciah18-meet <ahandal@js-bethlehem.com>
2017-03-13 19:59:13 +0200

    started python for loop practice

---
+++ for_loop_practice.py
@@ -0,0 +1,13 @@
+import turtle
+
+def print_triangle(n_lines):
+    for i in range (n_lines):
+        print(i*"*")
+        
+print_triangle(5)
+
+print_triangle(10)
+
+print_triangle(15)
+
+my_drawing
